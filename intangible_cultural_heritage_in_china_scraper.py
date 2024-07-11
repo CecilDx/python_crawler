f1149862536c474a8a84616a51740f37c57db7bb
CecilDx <87323786+CecilDx@users.noreply.github.com>
2024-07-11 21:21:10 +0800

    Update intangible_cultural_heritage_in_china_scraper.py

--- intangible_cultural_heritage_in_china_scraper.py
+++ intangible_cultural_heritage_in_china_scraper.py
@@ -8,7 +8,7 @@ class intangible_cultural_heritage(object):
 
     def __init__(self, pagenum):
         self.url = 'https://www.ihchina.cn/getProject.html?province=&rx_time=&type=&cate=&keywords=&category_id=16&limit=10&p={}'.format(pagenum)
-        self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+        self.headers = {'User-Agent':' '} # 输入浏览器 f12 中的 user-agent
         self.query = ['$..list..title', '$..list..type', '$..list..protect_unit', '$..list..province']
 
     def get_url(self):
@@ -24,17 +24,17 @@ class intangible_cultural_heritage(object):
 
     def write_to_mysql(self,data):
         conn = pymysql.connect(
-            host='localhost',
-            port=3306,
-            user='root',
-            password='PHX1103p',
-            database='requestsproject',
+            host='',
+            port=,
+            user='',
+            password='',
+            database='',
             charset='utf8'
         )
         try:
             cur = conn.cursor()
             for i in data:
-                sql = "insert into feiyiscraper (title, type, protect_unit, province) values (%s, %s, %s, %s)"
+                sql = "insert into intangible_cultural_heritage (title, type, protect_unit, province) values (%s, %s, %s, %s)"
                 cur.execute(sql, (i[0], i[1], i[2], i[3]))
             conn.commit()
         except Exception as e:
