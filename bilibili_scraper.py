5b918bec89952958f10e619e89533a73d481ed56
CecilDx <87323786+CecilDx@users.noreply.github.com>
2024-07-11 21:22:39 +0800

    Update bilibili_scraper.py

--- bilibili_scraper.py
+++ bilibili_scraper.py
@@ -11,8 +11,8 @@ class bilibili_scraper(object):
     def __init__(self, BV):
         self.url = 'https://www.bilibili.com/video/{}/'.format(BV)
         self.header = {'Referer': self.url,
-                       'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
-        self.cookies = {"Cookie":"buvid3=F197ADA4-DFCC-39C6-4A45-ECC55DEDF34256889infoc; b_nut=1720098956; CURRENT_FNVAL=4048; _uuid=DC83F5FE-35BB-1A102-8642-DFB4BED27B6656472infoc; buvid_fp=29ebe4633dfaa6f047f91621a71d937e; buvid4=CD951564-0156-5A11-E409-17BFF06FCDB457565-024070413-pehBMGu8tP8BJvPpPsBS9w%3D%3D; rpdid=|(u))|lkYYY)0J'u~k||m)ml); b_lsid=E95253C5_190A0EC35FA; bsource=search_bing; bmg_af_switch=1; bmg_src_def_domain=i2.hdslb.com; enable_web_push=DISABLE; header_theme_version=CLOSE; home_feed_column=5; browser_resolution=1707-932; SESSDATA=5dc797bb%2C1736238770%2C7b5ec%2A72CjCiNZ_WhfY53Hfd2Uk2aQFdpH5eQhynkxmpzrJU-EKz2D_kVDR_9nUDP7KWkf39jPMSVm8zSzVzVTJLOFlDYWdRRVhUR09nLWpkc3phS0w1LXNnaFlaczFNZ1JHVkY4RlJxaFRBRzlXN2U1czNZT0cxVjd6Zk9xbnlXWnNSWkhCdGtxU3NtZEZBIIEC; bili_jct=d66e9e1232a99556e2cbeeb812caf443; DedeUserID=4422245; DedeUserID__ckMd5=eea32aadb5496aea; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjA5NDU5NzgsImlhdCI6MTcyMDY4NjcxOCwicGx0IjotMX0.ojpTrwPmp8PWJ0JaO4RVk9l0ByX0qSmalaZ4hEl8JaY; bili_ticket_expires=1720945918; sid=6d53dg8c"}
+                       'User-Agent':''}
+        self.cookies = {"Cookie":""}
 
     def get_url(self):
         html = requests.get(self.url, headers=self.header, cookies=self.cookies)
